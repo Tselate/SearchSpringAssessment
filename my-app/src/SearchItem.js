a0685fea43ec387b34c3703e6e3300121dc47be6
Tselate <Tselatedawit@outlook.com>
2021-11-23 16:22:55 -0700

    Initial deploy

--- my-app/src/SearchItem.js
+++ my-app/src/SearchItem.js
@@ -10,7 +10,7 @@ function SearchItem () {
     const itemsStored = JSON.parse(sessionStorage.getItem("itemsKey")) || []
    
   
-
+    // Form submission will call this asyn function to fetch SearchSpring API data and set it to ItemResults and Pagination 
     const itemLookUp = async (e) => {
         e.preventDefault()
 
@@ -21,28 +21,57 @@ function SearchItem () {
             const data = await result.json()
             setItemResults(data.results)
             setPagination(data.pagination)
-          
             
         }catch(err) {
             alert("Item not found")
         }
-
-      
     }
 
+
+    // Updated items stored in session storage
     useEffect(() => {
        if (itemResults) {
         itemsStored.splice(0, 1, itemResults)
        }
         sessionStorage.setItem("itemsKey", JSON.stringify(itemsStored))
         sessionStorage.setItem("paginationKey", JSON.stringify(pagination))
-       
-
+        
     },[itemResults, itemsStored, pagination])
 
-    let paginationArray = []
- 
+    
 
+   
+    //Pagination of page will be determind based on what is inside the array below, along with min and max page numbers to be displayed 
+    let paginationArray = []
+
+    if(paginationStorage) {
+        let maxPage = paginationStorage.currentPage + 2
+        let minPage = paginationStorage.currentPage - 2
+    
+        if (minPage < 1) {
+            minPage = 1
+            maxPage = paginationStorage.totalPages
+        }
+    
+        if (maxPage > paginationStorage.totalPages) {
+            minPage = paginationStorage.totalPages - 4
+             
+            if (minPage < 1) {
+                minPage = 1
+            }
+    
+            maxPage = paginationStorage.totalPages
+        }
+
+
+         // Based on what is in the session storage, push all of the page numbers to the paginationArray
+
+        for(let i = minPage; i <= maxPage; i++) {
+            paginationArray.push(i)
+        }
+    }
+
+    //Function to grab value of button clicked and set it to the page number that is to be searched and displayed 
     function target (e) {
         if(e.target.value === "<") {
             if(paginationStorage.currentPage === 1) {
@@ -53,51 +82,29 @@ function SearchItem () {
         }else if(e.target.value !== "<" || e.target.value !== ">") {
             setSearchPage(e.target.value)
         }
-        
     }
 
+    
 
 
-
-
-   
+    // Based on what is in the session storage, push all of the page numbers to the paginationArray
     if(paginationStorage) {
-        for(let i = paginationStorage.currentPage; i <= paginationStorage.currentPage + 4 && i <= paginationStorage.totalPages ; i++) {
-            paginationArray.push(i)
-        }
+        
 
-        if(paginationStorage.currentPage !== paginationStorage.totalPagespaginationStorage) {
-            paginationArray.unshift("<")
-        }
+        // if(paginationStorage.currentPage !== paginationStorage.totalPagespaginationStorage) {
+        //     paginationArray.unshift("<")
+        // }
 
-        if (paginationStorage.currentPage !== paginationStorage.totalPages) {
-            paginationArray.push(">")
-        }
+        // if (paginationStorage.currentPage !== paginationStorage.totalPages) {
+        //     paginationArray.push(">")
+        // }
         
     }
 
-    
-    function paginationFunc() {
+    //****PAGE DISPLAY*****//
 
-        
-    
-        if (paginationStorage.currentPage > 1) {
-            paginationArray.unshift(<button  className="pageBtn"  onClick={target} value={paginationStorage.previousPage}>&lt;</button>)
-        }
-    
-        if(paginationStorage.currentPage !== paginationStorage.totalPages) {
-            paginationArray.push(<button  className="pageBtn"  onClick={target} value={paginationStorage.nextPage}>&gt;</button>)
-        }
-    
-        console.log(paginationArray)
-
-   }
-   
-    
-
-    
-
-    return (
+    if(itemResults && itemsStored[0]) {
+        return (
         <div className="container"> 
            <div className="form-container" >
                <h6 className="logo">SearchSpring</h6>
@@ -116,7 +123,7 @@ function SearchItem () {
                </form>               
             </div>    
 
-           <div className="paginationContainer"> <h6>Showing 25 results</h6> 
+           <div className="paginationContainer"> <h6>Showing {paginationStorage.perPage} results</h6> 
                     <form onSubmit={itemLookUp} >
                         {paginationArray.map(arr => (
                             <>
@@ -134,35 +141,73 @@ function SearchItem () {
                         ))}
 
                     </form>
-                   
-        
            </div>
 
-        
-
-                 {/* <div className="resultDisplay">
+    
+           <div className="resultDisplay">
                     {itemsStored[0].map(item => (
                         <div className="itemCard">
                             <div className="itemInfo">
                                 <img className="itemImg" src={item.imageUrl} alt="Item"/>
                                 <p className="itemName">{item.name}</p>
+                                <p className="itemMsrp">${item.msrp}.00</p>
                                 <p className="itemPrice">${item.price}.00</p>
                             </div>
                         </div>
                      
                     ))}
 
-               </div> */}
+               </div>
 
-                <div className="paginationContainer"> <h6>Showing 25 results</h6> 
-                   
-        
-           </div>
+               <div className="paginationContainer"> 
+                    <form onSubmit={itemLookUp} >
+                        {paginationArray.map(arr => (
+                            <>
+                                <button
+                                    type="submit"
+                                    name="subBtn"
+                                    className="btn"
+                                    value={arr}
+                                    onClick={target} 
+                                >
+                                    {arr}
+                                </button>
+                                
+                            </>
+                        ))}
 
-                 
-        
+                    </form>
+           </div>    
         </div>
     )
+    }else {
+        return (
+        <div className="container"> 
+           <div className="form-container" >
+               <h6 className="logo">SearchSpring</h6>
+               <form name="myForm" className="search-form" onSubmit={itemLookUp}>
+                   <input 
+                        type="text" 
+                        name="item"
+                        placeholder="Search here..."
+                        className="searchInput"
+                        required 
+                        value={item}
+                        onChange= {(e) => setItem(e.target.value)}
+                    /> 
+
+                    <button className="searchBtn" type="submit">Search</button>
+               </form>             
+            </div>
+
+            <h1 className="initialMessage">Search for any item!!</h1> 
+        </div>    
+        )
+    }
 }
+
+
+
+    
 
 export default SearchItem
