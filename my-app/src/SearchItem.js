a70be7c5802d9f537ad95daf83b3041a8ea51a82
Tselate <Tselatedawit@outlook.com>
2021-11-25 22:49:29 -0700

    Logic finished

--- my-app/src/SearchItem.js
+++ my-app/src/SearchItem.js
@@ -2,12 +2,14 @@ import React, {useState, useEffect} from "react"
 
 function SearchItem () {
     const [item, setItem] = useState("")
+    const [itemSearched, setItemSearched] = useState("")
     const [itemResults, setItemResults] = useState("")
     const [pagination, setPagination] = useState("")
     const [searchPage, setSearchPage] = useState("")
 
     const paginationStorage = JSON.parse(sessionStorage.getItem("paginationKey"))
     const itemsStored = JSON.parse(sessionStorage.getItem("itemsKey")) || []
+    const savedItem = JSON.parse(sessionStorage.getItem("searchKey"))
    
   
     // Form submission will call this asyn function to fetch SearchSpring API data and set it to ItemResults and Pagination 
@@ -24,9 +26,10 @@ function SearchItem () {
         }catch(err) {
             alert("Item not found")
         }
+        setItemSearched(item !== "" ? item : savedItem)
     }
 
-
+ 
     // Updated items stored in session storage
     useEffect(() => {
        if (itemResults) {
@@ -40,18 +43,21 @@ function SearchItem () {
     }, [item])
 
 
-
+    // Save pagination and searched item in session storage
     if(itemResults) {
         sessionStorage.setItem("paginationKey", JSON.stringify(pagination))
+        sessionStorage.setItem("searchKey", JSON.stringify(itemSearched))
     }
 
 
     //Pagination of page will be determind based on what is inside the array below, along with min and max page numbers to be displayed 
     let paginationArray = []
+   
 
     if(paginationStorage) {
-        let maxPage = paginationStorage.currentPage + 2
-        let minPage = paginationStorage.currentPage - 2
+        let maxPage = paginationStorage.totalPages
+        let maxRight = paginationStorage.currentPage + 2 
+        let minPage = paginationStorage.currentPage - 2 
     
         if (minPage < 1) {
             minPage = 1
@@ -60,7 +66,7 @@ function SearchItem () {
     
         if (paginationStorage.currentPage === paginationStorage.totalPages) {
             minPage = paginationStorage.totalPages - 4
-             
+
             if (minPage < 1) {
                 minPage = 1
             }
@@ -70,18 +76,25 @@ function SearchItem () {
 
          // Based on what is in the session storage, push all of the page numbers to the paginationArray
 
-        for(let i = minPage; i <= maxPage; i++) {
+        for(let i = minPage; i <= maxRight; i++) {
+            if (maxRight >= maxPage) {
+                maxRight = paginationStorage.totalPages
+            }
             paginationArray.push(i)
         }
 
         if(paginationStorage.currentPage !== 1) {
-            paginationArray.unshift("<")
+            paginationArray.unshift("<")  
         }
 
         if (paginationStorage.currentPage !== paginationStorage.totalPages) {
-            paginationArray.push(">")
+            paginationArray.push(">")  
         }
+        
     }
+
+    
+   
 
     //Function to grab value of button clicked and set it to the page number that is to be searched and displayed 
     function target (e) {
@@ -100,13 +113,19 @@ function SearchItem () {
         }else if(e.target.value !== "<" || e.target.value !== ">") {
             setSearchPage(e.target.value)
         }
+        setItem(savedItem)
     }
 
 
+   
+   
+
+    
+  
 
     //****PAGE DISPLAY*****//
 
-    if((itemResults && itemsStored[0]) || itemsStored.length > 1) {
+    if(itemsStored[0]) {
         return (
         <div className="container"> 
            <div className="form-container" >
@@ -131,6 +150,7 @@ function SearchItem () {
                         {paginationArray.map(arr => (
                             <>
                                 <button
+                                    key={arr.value}
                                     type="submit"
                                     name="subBtn"
                                     className="btn"
@@ -150,11 +170,12 @@ function SearchItem () {
            <div className="resultDisplay">
                     {itemsStored[0].map(item => (
                         <div className="itemCard">
-                            <div className="itemInfo">
+                            <div className="itemInfo" key={item.uid}>
                                 <img className="itemImg" src={item.imageUrl} alt="Item"/>
                                 <p className="itemName">{item.name}</p>
-                                <p className="itemMsrp">${item.msrp}.00</p>
-                                <p className="itemPrice">${item.price}</p>
+                                {/* <p className="itemMsrp">${item.msrp > }</p> */}
+                                {item.msrp > item.price ? <p className="itemMsrp">${item.msrp}.00</p> : null}
+                                <p className="itemPrice">${item.price % 1 !== 0 ? `${item.price}0`: `${item.price}.00`}</p>
                             </div>
                         </div>
                      
@@ -167,6 +188,7 @@ function SearchItem () {
                         {paginationArray.map(arr => (
                             <>
                                 <button
+                                    key={arr.value}
                                     type="submit"
                                     name="subBtn"
                                     className="btn"
